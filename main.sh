0f90216b8a3fddc09e1367e3215246baf59d948c
uberrotaran001 <71384223+uberrotaran001@users.noreply.github.com>
2022-06-21 15:10:32 +0200

    Añadido el main

---
+++ main.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$1" = "ext4" -o "$1" = "BeeGFS" ]
+then
+  CONTENEDOR="cont21"
+  SCRIPT_BENCHMARKS="benchmarks.sh"
+  EJECUTABLE_BENCHMARK="aio-stress"
+
+  sh $SCRIPT_BENCHMARKS 0 $EJECUTABLE_BENCHMARK $1
+  singularity exec $CONTENEDOR sh $SCRIPT_BENCHMARKS 1 $EJECUTABLE_BENCHMARK $1
+
+  #sh metricas1.sh
+else 
+  echo "Error de formato: sh $0 ext4|BeeGFS"
+fi
